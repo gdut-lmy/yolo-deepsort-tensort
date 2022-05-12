a0e21a24f8b6e85aed8310f1892d0f2e4e3e0a31
lmy <1429185804@qq.com>
2022-05-12 17:25:15 +0800

    successfully run the project

---
+++ cmake-build-debug/CMakeFiles/yolosort.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yolosort.dir/src/main.cpp.o"
+  "CMakeFiles/yolosort.dir/src/manager.cpp.o"
+  "yolosort"
+  "yolosort.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/yolosort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/yolov5_trt.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yolov5_trt.dir/yolo/include/yolov5_trt_generated_yololayer.cu.o"
+  "CMakeFiles/yolov5_trt.dir/yolo/src/yolov5_lib.cpp.o"
+  "libyolov5_trt.pdb"
+  "libyolov5_trt.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/yolov5_trt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
