c6f43b5105c890e799fa9daa5924685f69813f26
[gdut-lmy] <[1429185804@qq.com]>
2022-06-14 12:00:17 +0800

    add  realsenseD455 class and threadPool-C++11

--- include/objectProcess.h
+++ include/objectProcess.h
@@ -5,15 +5,15 @@
 #ifndef YOLOSORT_OBJECTANGLE_H
 #define YOLOSORT_OBJECTANGLE_H
 
-#include <realsense_config.h>
 #include <manager.hpp>
 #define pi 3.14159
-
+const float depth_scale = 0.001;
 void dealWithDistance(float &dis,int x,int y,float angle);
 
 
 void getObjectAngleFromPixel(DetectBox &box);
 void getObjectAngle(DetectBox &box);
+float measure_distance(const cv::Mat &depth,const DetectBox &box,const cv::Size& range);
 
 bool isValidBox(DetectBox box);
 
--- src/objectProcess.cpp
+++ src/objectProcess.cpp
@@ -30,7 +30,7 @@ void getObjectAngleFromPixel(DetectBox &box) {
 void getObjectAngleFrom3D(DetectBox &box) {
     float tmp = atan(box.pdc[0] / box.pdc[2]);
     box.angle = tmp * 180 / pi;
-    cout << "The Object angle:" << box.angle << endl;
+    //cout << "The Object angle:" << box.angle << endl;
 }
 
 void dealWithDistance(float &dis, int x, int y, float angle) {
@@ -55,6 +55,36 @@ bool isValidBox(DetectBox box) {
 
 void trackBox(DetectBox box) {
 
-
+    cout << "trackBox.dis ----------------------:" << box.dis << endl;
     cout << "trackBox ----------------------:" << box.trackID << endl;
+}
+
+
+float measure_distance(const cv::Mat &depth, const DetectBox &box, const cv::Size &range)//声明profile
+{
+    //定义图像中心点
+    cv::Point center(box.pixel_x, box.pixel_y);
+    //定义计算距离的范围
+    cv::Rect RectRange(center.x - range.width / 2, center.y - range.height / 2, range.width, range.height);
+    //std::cout<<box.x1<<" " <<box.y1<<" "<<box.x2 <<" "<<box.y2 <<std::endl;
+    //画出范围
+    float distance_sum = 0;
+    int effective_pixel = 0;
+
+    for (int y = RectRange.y; y < RectRange.y + RectRange.height; y++) {
+        for (int x = RectRange.x; x < RectRange.x + RectRange.width; x++) {
+            //不是0就有位置信息
+            if (depth.at<uint16_t>(y, x))//出现位置信息
+            {
+                distance_sum += depth_scale * depth.at<uint16_t>(y, x);
+                effective_pixel++;
+            }
+        }
+    }
+    //std::cout << "有效像素点：" << effective_pixel <<std::endl;//输出数据
+
+    float effective_distance = distance_sum / effective_pixel;
+    //std::cout << "目标距离：" << effective_distance << "m" << std::endl;
+
+    return effective_distance;
 }
